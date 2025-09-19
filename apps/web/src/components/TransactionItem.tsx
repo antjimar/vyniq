a561d21c4c9e10f7d7b7e6cb6a8d2ebad0f0b7ac
antjimar <173185650+appStackiOS@users.noreply.github.com>
2025-09-19 07:28:01 +0200

    feat: react basics learnings

---
+++ apps/web/src/components/TransactionItem.tsx
@@ -0,0 +1,8 @@
+export default function TransactionItem() {
+    console.log("TransactionItem ejecutándose")
+    return (
+        <div>
+            <p>Transacción de ejemplo</p>
+        </div>
+    )
+}
